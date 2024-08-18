21c8c9956294049f5c973bbd62c3151f6273ec75
goracy <jakubgoracy@student.agh.edu.pl>
2024-08-18 21:21:49 +0200

    gitignore

---
+++ target/debug/deps/liqudity_pool-cbe961a67209f9ed.d
@@ -0,0 +1,7 @@
+/home/goro/Desktop/liqudity_pool/target/debug/deps/liqudity_pool-cbe961a67209f9ed: src/main.rs src/calc.rs src/errors.rs
+
+/home/goro/Desktop/liqudity_pool/target/debug/deps/liqudity_pool-cbe961a67209f9ed.d: src/main.rs src/calc.rs src/errors.rs
+
+src/main.rs:
+src/calc.rs:
+src/errors.rs:
