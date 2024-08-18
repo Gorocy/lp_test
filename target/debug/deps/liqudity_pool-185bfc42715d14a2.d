573e3cb95c83a9581b78ee9ed869f01983c071a3
goracy <jakubgoracy@student.agh.edu.pl>
2024-08-18 21:24:55 +0200

    remove target

--- target/debug/deps/liqudity_pool-185bfc42715d14a2.d
+++
@@ -1,7 +0,0 @@
-/home/goro/Desktop/liqudity_pool/target/debug/deps/liqudity_pool-185bfc42715d14a2: src/main.rs src/calc.rs src/errors.rs
-
-/home/goro/Desktop/liqudity_pool/target/debug/deps/liqudity_pool-185bfc42715d14a2.d: src/main.rs src/calc.rs src/errors.rs
-
-src/main.rs:
-src/calc.rs:
-src/errors.rs:
--- target/debug/liqudity_pool.d
+++
@@ -1,1 +0,0 @@
-/home/goro/Desktop/liqudity_pool/target/debug/liqudity_pool: /home/goro/Desktop/liqudity_pool/src/calc.rs /home/goro/Desktop/liqudity_pool/src/errors.rs /home/goro/Desktop/liqudity_pool/src/main.rs
